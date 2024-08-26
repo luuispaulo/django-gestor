70dbcaec6dc89c9652a0507c9a19aa4b362208cc
Your Name <you@example.com>
2024-08-26 13:42:36 -0300

    shopee

--- shopee/urls.py
+++ shopee/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include, reverse_lazy
-from shopee import views 
+from shopee.views import views_auth 
 
 app_name = 'shopee'
 
 urlpatterns = [
-    path('shopee/auth/authorize/',views.authorize ,name='authorize_shopee'),
-    path('shopee/auth/callback/',views.callback ,name='callback_shopee'),
-    path('shopee/auth/new_refresh_token/',views.refresh_token ,name='refresh_token_shopee'),
+    path('shopee/auth/authorize/',views_auth.authorize ,name='authorize_shopee'),
+    path('shopee/auth/callback/',views_auth.callback ,name='callback_shopee'),
+    path('shopee/auth/new_refresh_token/',views_auth.refresh_token ,name='refresh_token_shopee'),
 ]
